e8ccbfb3b6bde3ecd1d6dabab68cca878d4df5da
Andrei Atrakhimovich <atrakhimovich95@gmail.com>
2026-04-16 19:53:10 +0300

    Добавил пароль

--- Lesson3_GitTest/ViewController.swift
+++ Lesson3_GitTest/ViewController.swift
@@ -10,12 +10,12 @@ import UIKit
 class ViewController: UIViewController {
 
     let username = ""
+    let password = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 
 }
