93e00fa9a1f07f642c4942f0eb647473ca413f8a
Andrei Atrakhimovich <atrakhimovich95@gmail.com>
2026-04-16 19:52:19 +0300

    Добавил новую фичу на экран X

--- Lesson3_GitTest/ViewController.swift
+++ Lesson3_GitTest/ViewController.swift
@@ -9,10 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let username = ""
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+
 
 
 }
